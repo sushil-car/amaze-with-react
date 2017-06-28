19a41ba7b55a4623604cb7e140ad49510da7d2f2
Sushil Adokar <sushiladokar@gmail.com>
2017-06-28 18:10:51 +0530

    Amaze

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require("webpack");
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+    entry: SRC_DIR + '/app/index.js',
+    output: {
+        path: DIST_DIR + '/app',
+        filename: 'bundle.js',
+        publicPath: '/app'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: SRC_DIR,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env']
+            }
+          }
+        }
+      ]
+    }
+};
+
+module.exports = config;
